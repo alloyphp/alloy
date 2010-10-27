9c3af67b185b7c1153d4a3d4268def0dd9116544
Vance Lucas <vance@vancelucas.com>
2010-10-27 13:50:39 -0500

    lib > Alloy: New base Controller in 'App' namespace for user-created things to extend from

---
+++ lib/App/Module/ControllerAbstract.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Module;
+
+/**
+ * Base abstract 'App' module
+ * Extend all your application controllers from this and add custom functionality here
+ */
+class ControllerAbstract extends \Alloy\Module\ControllerAbstract {}
