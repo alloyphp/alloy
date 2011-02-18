3f8ff849615d7805b559978649bf81f9c281b742
Vance Lucas <vance@vancelucas.com>
2011-02-18 12:07:10 -0600

    Resource: Added ability to set and send errors, and added ability to retrieve set status code and other content

--- lib/Alloy/Resource.php
+++ lib/Alloy/Resource.php
@@ -10,6 +10,7 @@ namespace Alloy;
 class Resource
 {
     protected $_resource;
+    protected $_errors = array();
     protected $_location;
     protected $_status = 200;
     
@@ -24,16 +25,32 @@ class Resource
         $this->_resource = $resource;
     }
     
-    
+
     /**
      * URL location to be returned with resource
      *
      * @param string $url URL of the resource to be returned in headers
      */
-    public function location($url)
+    public function created($url, $status = 201)
     {
         $this->_location = $url;
-        $this->status(201);
+        $this->status($status);
+        return $this;
+    }
+
+
+    /**
+     * URL location to be returned with resource
+     *
+     * @param string $url URL of the resource to be returned in headers
+     */
+    public function location($url = null)
+    {
+        if(null === $url) {
+            return $this->_location;
+        }
+
+        $this->_location = $url;
         return $this;
     }
     
@@ -43,9 +60,29 @@ class Resource
      *
      * @param int $statusCode HTTP status code to return
      */
-    public function status($statusCode = 200)
+    public function status($statusCode = null)
     {
+        if(null === $statusCode) {
+            return $this->_status;
+        }
+
         $this->_status = $statusCode;
+        return $this;
+    }
+
+
+    /**
+     * Errors
+     *
+     * @param mixed $errors Error array to return
+     */
+    public function errors($errors = null)
+    {
+        if(null === $errors) {
+            return $this->_errors;
+        }
+
+        $this->_errors += $errors;
         return $this;
     }
     
@@ -58,27 +95,46 @@ class Resource
      */
     public function toArray()
     {
+        $r = false;
+
+        // Already array
         if(is_array($this->_resource)) {
-            return $this->_resource;
+            $r = $this->_resource;
     
         // String
         } elseif(is_string($this->_resource)) {
             // JSON decode
             if($json = json_decode($this->_resource)) {
-                return $json;
+                $r = $json;
             // Unserialize
             } elseif($data = unserialize($this->_resource)) {
-                return $data;
+                $r = $data;
             }
         
         // Object
         } elseif(is_object($this->_resource)) {
             // Call 'toArray' on object
             if(method_exists($this->_resource, 'toArray')) {
-                return $this->_resource->toArray();
+                $r = $this->_resource->toArray();
             }
         }
-        throw new Alloy_Exception("Resource (" . gettype($this->_resource) . ") could not be converted to array");
+
+        // Ensure we do have an array
+        if(!is_array($r)) {
+            throw new Exception("Resource (" . gettype($this->_resource) . ") could not be converted to array");
+        }
+
+        // Put results in names key
+        if(!isset($r['results'])) {
+            $r = array('results' => $r);
+        }
+
+        // Set/add errors, if any
+        if($errors = $this->errors()) {
+            $r['errors'] = $errors;
+        }
+
+        return $r;
     }
     
     /**
@@ -92,7 +148,12 @@ class Resource
         if(is_string($this->_resource)) {
             return $this->_resource;
         }
-        // Else return JSON encoded data
-        return json_encode($this->toArray());
+
+        try {
+            // Else return JSON encoded data
+            return json_encode($this->toArray());
+        } catch(\Exception $e) {
+            return $e->getMessage();
+        }
     }
 }
