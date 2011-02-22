07782e7c3a8edafcb620f7d12f62a24062c46fd1
Vance Lucas <vance@vancelucas.com>
2011-02-22 16:12:08 -0600

    Form Generic: Updated to new view code

--- lib/Alloy/View/Generic/templates/form.html.php
+++ lib/Alloy/View/Generic/templates/form.html.php
@@ -1,14 +1,14 @@
 <?php
-$form = $this->helper('Form');
-$formMethod = strtoupper(($this->method == 'GET' || $this->method == 'POST') ? $this->method : 'post');
-$formMethodRest = ($formMethod == 'POST' && $this->method != 'POST') ? $this->method : false;
+$form = $view->helper('Form');
+$formMethod = strtoupper(($method == 'GET' || $method == 'POST') ? $method : 'post');
+$formMethodRest = ($formMethod == 'POST' && $method != 'POST') ? $method : false;
 ?>
 
-<form action="<?php echo $this->action; ?>" method="post">
+<form action="<?php echo $action; ?>" method="post">
   <ol class="app_form">
-  <?php if($this->fields && count($this->fields) >0): ?>
+  <?php if($fields && count($fields) >0): ?>
   <?php
-  foreach($this->fields as $fieldName => $fieldOpts):
+  foreach($fields as $fieldName => $fieldOpts):
     $fieldLabel = isset($fieldOpts['title']) ? $fieldOpts['title'] : ucwords(str_replace('_', ' ', $fieldName));
     $fieldType = isset($fieldOpts['type']) ? $fieldOpts['type'] : 'string';
     ?>
@@ -21,34 +21,34 @@ $formMethodRest = ($formMethod == 'POST' && $this->method != 'POST') ? $this->me
         case 'text':
         case 'editor':
           $attrs = array('rows' => 10, 'cols' => 60);
-          echo $form->textarea($fieldName, $this->data($fieldName), $attrs);
+          echo $form->textarea($fieldName, $view->data($fieldName), $attrs);
         break;
         
         case 'bool':
         case 'boolean':
-          echo $form->checkbox($fieldName, (int) $this->data($fieldName));
+          echo $form->checkbox($fieldName, (int) $view->data($fieldName));
         break;
         
         case 'int':
         case 'integer':
-          echo $form->text($fieldName, $this->data($fieldName), array('size' => 10));
+          echo $form->text($fieldName, $view->data($fieldName), array('size' => 10));
         break;
         
         case 'string':
-          echo $form->text($fieldName, $this->data($fieldName), array('size' => 40));
+          echo $form->text($fieldName, $view->data($fieldName), array('size' => 40));
         break;
         
         case 'select':
           $options = isset($fieldOpts['options']) ? $fieldOpts['options'] : array();
-          echo $form->select($fieldName, $options, $this->data($fieldName));
+          echo $form->select($fieldName, $options, $view->data($fieldName));
         break;
         
         case 'password':
-          echo $form->input('password', $fieldName, $this->data($fieldName), array('size' => 25));
+          echo $form->input('password', $fieldName, $view->data($fieldName), array('size' => 25));
         break;
         
         default:
-          echo $form->input($fieldType, $fieldName, $this->data($fieldName));
+          echo $form->input($fieldType, $fieldName, $view->data($fieldName));
       }
       ?>
       </span>
@@ -58,8 +58,8 @@ $formMethodRest = ($formMethod == 'POST' && $this->method != 'POST') ? $this->me
     <li class="app_form_hidden">
       <?php
       // Print out set data without fields as hidden fields in form
-      $setData = $this->data();
-      $setFields = $this->fields();
+      $setData = $view->data();
+      $setFields = $view->fields();
       $dataWithoutFields = array_diff_key($setData, $setFields);
       foreach($dataWithoutFields as $unsetField => $unsetValue):
       ?>
@@ -72,7 +72,7 @@ $formMethodRest = ($formMethod == 'POST' && $this->method != 'POST') ? $this->me
       <?php endif; ?>
     </li>
     <li class="app_form_actions">
-      <button type="submit" class="app_action_primary"><?php echo $this->submitButtonText(); ?></button>
+      <button type="submit" class="app_action_primary"><?php echo $view->submitButtonText(); ?></button>
       <!--<a href="#" class="app_action_cancel">Cancel</a>-->
     </li>
   </ol>
