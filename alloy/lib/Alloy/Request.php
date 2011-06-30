dd05799f57a033ecc0e8f71639fb6e215b76df93
Vance Lucas <vance@vancelucas.com>
2011-06-30 09:22:14 -0500

    lib > Request: Updated to account for magic_quotes on and dot-separated requests variable getting (nested arrays)

--- alloy/lib/Alloy/Request.php
+++ alloy/lib/Alloy/Request.php
@@ -15,6 +15,24 @@ class Request
 {
     // Request parameters
     protected $_params = array();
+
+
+    /**
+     * Ensure magic quotes are not mucking up request data
+     */
+    public function __construct()
+    {
+        // Die magic_quotes, just die...
+        if(get_magic_quotes_gpc()) {
+            $stripslashes_gpc = function(&$value, $key) {
+                $value = stripslashes($value);
+            };
+            array_walk_recursive($_GET, $stripslashes_gpc);
+            array_walk_recursive($_POST, $stripslashes_gpc);
+            array_walk_recursive($_COOKIE, $stripslashes_gpc);
+            array_walk_recursive($_REQUEST, $stripslashes_gpc);
+        }
+    }
     
     
     /**
@@ -30,30 +48,56 @@ class Request
         switch (true) {
             case isset($this->_params[$key]):
                 $value = $this->_params[$key];
-            break;
+                break;
             
             case isset($_GET[$key]):
                 $value = $_GET[$key];
-            break;
+                break;
             
             case isset($_POST[$key]):
                 $value = $_POST[$key];
-            break;
+                break;
             
             case isset($_COOKIE[$key]):
                 $value = $_COOKIE[$key];
-            break;
+                break;
             
             case isset($_SERVER[$key]):
                 $value = $_SERVER[$key];
-            break;
+                break;
                 
             case isset($_ENV[$key]):
-                    $value = $_ENV[$key];
-            break;
+                $value = $_ENV[$key];
+                break;
                 
             default:
-            return $default;
+                $value = $default;
+        }
+
+        // Key not found, default is being used
+        if($value === $default) {
+            // Check for dot-separator (convenience access for nested array values)
+            if(strpos($key, '.') !== false) {
+                // Get all dot-separated key parts
+                $keyParts = explode('.', $key);
+
+                // Remove first key because we're going to start with it
+                $keyFirst = array_shift($keyParts);
+
+                // Get root value array to begin
+                $value = $this->get($keyFirst);
+
+                // Loop over remaining key parts to see if value can be found in resulting array
+                foreach($keyParts as $keyPart) {
+                    if(is_array($value)) {
+                        if(isset($value[$keyPart])) {
+                            $value = $value[$keyPart];
+                        } else {
+                            $value = $default;
+                        }   
+                    }
+                }
+            }
         }
         
         return $value;
@@ -526,28 +570,5 @@ class Request
     public function isFlash()
     {
         return ($this->header('USER_AGENT') == 'Shockwave Flash');
-    }
-    
-    
-    /**
-     * Apply a user-defined function to all request parameters
-     *
-     * @string $function user-defined function
-     */
-    public function map($function)
-    {
-        $in = array(&$_GET, &$_POST, &$_COOKIE);
-        while (list($k,$v) = each($in)) {
-            if(is_array($v)) {
-                foreach ($v as $key => $val) {
-                    if (!is_array($val)) {
-                        $in[$k][$key] = $function($val);
-                    }
-                    $in[] =& $in[$k][$key];
-                }
-            }
-        }
-        unset($in);
-        return true;
     }
 }
